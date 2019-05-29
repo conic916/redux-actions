870b188c253c7899743543c9be3fceda173ce10b
Katarzyna Da Silva <conic916@gmail.com>
2019-05-29 23:12:37 +0200

    Modified

--- action.js
+++ action.js
@@ -1,19 +1,57 @@
-// import uuid from uuid;
+import uuid from uuid;
 
+const ADD_COMMENT = 'ADD_COMMENT';
 const REMOVE_COMMENT = 'REMOVE_COMMENT';
 const EDIT_COMMENT = 'EDIT_COMMENT';
 const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-const boundREMOVE_COMMENT = text => dispatch(REMOVE_COMMENT(text));
-boundREMOVE_COMMENT('...');
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4(),
+        rating: 0
+    }
+}
 
-const boundEDIT_COMMENT = text => dispatch(EDIT_COMMENT(text));
-boundEDIT_COMMENT('...');
+function editComment(id, text) {
+    return {
+        type: ADD_COMMENT, // EDIT
+        id,
+        text
+    }
+}
 
-const boundTHUMB_UP_COMMENT = id => dispatch(THUMB_UP_COMMENT(id));
-boundTHUMB_UP_COMMENT('...');
+function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id
+    }
+}
+function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id,
+        thumb: add +1
+    }
+}
 
-const boundTHUMB_DOWN_COMMENT = id => dispatch(THUMB_DOWN_COMMENT(id));
-boundTHUMB_DOWN_COMMENT('...');
+function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id,
+        thumb: removed -1 
+    }
+}
+// const boundREMOVE_COMMENT = text => dispatch(REMOVE_COMMENT(text));
+// boundREMOVE_COMMENT('...');
 
+// const boundEDIT_COMMENT = text => dispatch(EDIT_COMMENT(text));
+// boundEDIT_COMMENT('...');
+
+// const boundTHUMB_UP_COMMENT = id => dispatch(THUMB_UP_COMMENT(id));
+// boundTHUMB_UP_COMMENT('...');
+
+// const boundTHUMB_DOWN_COMMENT = id => dispatch(THUMB_DOWN_COMMENT(id));
+// boundTHUMB_DOWN_COMMENT('...');
